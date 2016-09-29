dad0af16434918e22a062ca56e33538671421cd6
padamsson <patricia.adamsson@creuna.se>
2016-09-29 18:45:05 +0200

    Render user name and age

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,7 +1,26 @@
 import React from "react"
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions/userActions';
+
+@connect((store) => {
+    return {
+      user: store.user.user,
+      userFetched: store.user.fetched,
+      tweets: store.tweets.tweets
+    }
+})
 
 export default class Layout extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchUser());
+  };
   render() {
-    return null;
+    const user = this.props.user;
+    return (
+      <div>
+        <h1>{user.name}</h1>
+        <h3>Age: {user.age}</h3>
+      </div>
+    );
   }
 }
