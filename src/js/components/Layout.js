fdcbc4ef355e5d9a41570506bbe7c4621763a6eb
padamsson <patricia.adamsson@creuna.se>
2016-09-29 18:57:29 +0200

    Fetch tweets

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions/userActions';
+import { fetchTweets } from '../actions/tweetsActions';
 
 @connect((store) => {
     return {
@@ -13,13 +14,24 @@ import { fetchUser } from '../actions/userActions';
 export default class Layout extends React.Component {
   componentWillMount() {
     this.props.dispatch(fetchUser());
-  };
+  }
+
+  fetchTweets() {
+    this.props.dispatch(fetchTweets());
+  }
+
   render() {
-    const user = this.props.user;
+    const { user, tweets } = this.props;
+    const mappedTweets = tweets.map(tweet => <li>{tweet.text}</li>);
+
+    if (!tweets.length) {
+      return <button onClick={this.fetchTweets.bind(this)}>Render tweets!</button>;
+    }
     return (
       <div>
         <h1>{user.name}</h1>
         <h3>Age: {user.age}</h3>
+        <ul>{mappedTweets}</ul>
       </div>
     );
   }
